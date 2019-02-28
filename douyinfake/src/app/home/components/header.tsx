a495e660c343b8dbfe82a7aa7c4fb069292c3b04
yangxiaodong <yangxiaodong@qianmi.com>
2019-02-28 21:55:43 +0800

    add: 添加起来..

--- douyinfake/src/app/home/components/header.tsx
+++ douyinfake/src/app/home/components/header.tsx
@@ -16,9 +16,17 @@ export default class Header extends Component<Props> {
   render() {
     return (
       <View style={style.container}>
-        <Text style={style.font}>首页</Text>
-        <Text style={style.font}>首页</Text>
-        <Text style={style.font}>首页</Text>
+        <View style={style.btn}>
+          <Text style={style.font}>随拍</Text>
+        </View>
+        <View style={style.btnCenter}>
+          <Text style={style.font}>推荐</Text>
+          <View style={style.split}></View>
+          <Text style={style.font}>南京</Text>
+        </View>
+        <View style={style.btn}>
+          <Text style={style.font}>search</Text>
+        </View>
       </View>
     );
   }
@@ -26,8 +34,25 @@ export default class Header extends Component<Props> {
 
 let style=StyleSheet.create({
   container:{
-    // flex:1,
-    // flexDirection:"row",
+    flexDirection:"row",
+    justifyContent:"space-around",
+  },
+
+  btn:{
+    width:80,
+    flexDirection:"row",
+    marginLeft:10,
+    marginRight:10,
+    justifyContent:"center"
+  },
+
+  btnCenter:{
+    flex:1,
+    flexDirection:"row",
+    justifyContent:"center"
+  },
+  split:{
+    width:15
   },
   font:{
     color:"white",
