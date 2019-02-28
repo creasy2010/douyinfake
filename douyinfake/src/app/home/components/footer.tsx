3c56c3c41bf6afba65289642cee638fc97074b07
yangxiaodong <yangxiaodong@qianmi.com>
2019-02-28 22:26:02 +0800

    add: 添加起来..

--- douyinfake/src/app/home/components/footer.tsx
+++ douyinfake/src/app/home/components/footer.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,Image} from 'react-native';
 
 /**
  * @desc
@@ -18,9 +18,48 @@ interface Props{
 export default class Footer extends Component<Props> {
   render() {
     return (
-      <View>
-        <Text>首页</Text>
+      <View style={styles.container}>
+        <View style={styles.subItem}>
+          <Text style={styles.font}>首页</Text>
+        </View>
+        <View style={styles.subItem}>
+          <Text style={styles.font}>好友</Text>
+        </View>
+        <View style={styles.subItem}>
+          {/*<Image></Image>*/}
+        </View>
+        <View style={styles.subItem}>
+          <Text style={styles.font}>消息</Text>
+        </View>
+        <View style={styles.subItem}>
+          <Text style={styles.font}>我的</Text>
+        </View>
       </View>
     );
   }
 }
+
+
+let styles=StyleSheet.create({
+
+  container:{
+    color:"white",
+    height:50,
+    flexDirection:"row",
+    bottom:50,
+  },
+
+  subItem:{
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"blue",
+    flex:1,
+  },
+  subItemActive:{
+    flex:1,
+  },
+  font:{
+    color:"white",
+    fontSize:16
+  }
+})
